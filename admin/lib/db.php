e453e96eface490d56cbcfe771034e78aa12a317
Kaisin <wongksin6@gmail.com>
2024-03-20 01:23:12 +0800

    Sin

---
+++ admin/lib/db.php
@@ -0,0 +1,11 @@
+<?php
+$dbName = 'bagel';
+$dbServer = 'localhost';
+$dbUser = '';
+$dbPass = '';
+
+$db_conn = mysqli_connect($dbServer, $dbUser, $dbPass, $dbName) or die("Connection not available.");
+mysqli_query($db_conn, "SET NAMES utf8") or die("Set names wrong.");
+
+date_default_timezone_set('Asia/Kuala_Lumpur');
+?>
