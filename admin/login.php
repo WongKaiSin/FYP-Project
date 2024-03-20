981578a95b31076cfc47656fd484c922fc99aeef
Kaisin <wongksin6@gmail.com>
2024-03-20 18:22:34 +0800

    Sin

--- admin/login.php
+++ admin/login.php
@@ -2,15 +2,7 @@
 <html lang="en">
  
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Login</title>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="../assets/vendor/bootstrap/css/bootstrap.min.css">
-    <link href="../assets/vendor/fonts/circular-std/style.css" rel="stylesheet">
-    <link rel="stylesheet" href="../assets/libs/css/style.css">
-    <link rel="stylesheet" href="../assets/vendor/fonts/fontawesome/css/fontawesome-all.css">
+    <?php include("lib/head.php"); ?>
     <style>
     html,
     body {
@@ -60,13 +52,8 @@
             </div>
         </div>
     </div>
-  
     <!-- ============================================================== -->
     <!-- end login page  -->
     <!-- ============================================================== -->
-    <!-- Optional JavaScript -->
-    <script src="../assets/vendor/jquery/jquery-3.3.1.min.js"></script>
-    <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
 </body>
- 
 </html>
