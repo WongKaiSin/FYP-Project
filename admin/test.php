a70ffcdd39a85519c8d05402292d11e5a041acdd
Kaisin <wongksin6@gmail.com>
2024-03-27 00:02:52 +0800

    Sin

--- admin/test.php
+++ admin/test.php
@@ -9,7 +9,15 @@ $pass = "kai"; // Change to a valid password
 
 $done = $func->PassSign($email, $pass);
 
+// var_dump('location: '.$_SERVER['DOCUMENT_ROOT']."/FYP-Project/admin/login.php");
+// header("location: ../user/menu-info.php");
+
+echo basename($_SERVER["SCRIPT_FILENAME"])."<br>";
+echo $_SERVER['DOCUMENT_ROOT']."/FYP-Project/<br>";
+
 echo "Email: ". $email."<br>";
 echo "Pass: ".$pass."<br>";
 echo "Hashed password: ".$done;
 ?>
+
+<script>location="<?$_SERVER['DOCUMENT_ROOT']."/FYP-Project/"?>"</script>
