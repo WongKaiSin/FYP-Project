0d6c51dbd08cd8425448c58418a88267b8bf8931
Kaisin <wongksin6@gmail.com>
2024-04-04 16:19:13 +0800

    Sin

---
+++ user/lib/function.php
@@ -0,0 +1,23 @@
+<?php
+
+class Functions
+{
+    function PassSign($email, $password) 
+    { 
+        return base64_encode(hex2bin(sha1($email . $password . "Ad2024@123"))); 
+    }
+
+    function PassSignCust($email, $password) 
+    { 
+        return base64_encode(hex2bin(sha1($email . $password . "Mem123@2024"))); 
+    }
+
+    function checkPass($pass)
+	{
+		$result = (!preg_match('/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])[0-9A-Za-z]{8,20}$/', $pass)) ? false : true;
+		return $result;
+	}
+    
+
+}
+?>
