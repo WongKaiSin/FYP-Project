2489aeb9ae1a7e477ed4fe8a83d93a3926f20f52
PuaZiSien <1211210639@student.mmu.edu.my>
2024-04-07 16:30:40 +0800

    Pua

--- user/forms/logout.php
+++ user/forms/logout.php
@@ -3,11 +3,14 @@
 
     // Check if the logout button is clicked
     if(isset($_POST['logout'])) {
-        session_start();
+        // Unset all session variables
+        $_SESSION = array();
+
+        // Destroy the session
         session_destroy();
 
         // Redirect to the login page or any other page after logout
-        header('location: ../index.php');
+        header('Location: ../index.php');
         exit;
     }
 ?>
--- user/lib/sidebar.php
+++ user/lib/sidebar.php
@@ -1,23 +1,21 @@
-<aside id="sidebar" class="sidebar">
-				  <h1>My Account</h1>
-				  <ul class='menu'>
-					  <li><a href="member_profile.php">Profile</a></li>
-					  <li><a href="member_password.php">Change Password</a></li>
-				  </ul>
-			  </aside>
-			
-<aside id="sidebar" class="sidebar">
-				  <h1>My Orders</h1>
-				  <ul class='menu'>
-					  <li><a href="$SiteUrl/my-waiting-orders">Booking</a></li>
-					  <li><a href="$SiteUrl/my-confirmed-orders">Order History</a></li>
-				  </ul>
-			  </aside>
+<aside id="sidebar_account" class="sidebar">
+    <h1>My Account</h1>
+    <ul class='menu'>
+        <li><a href="member_profile.php">Profile</a></li>
+        <li><a href="member_password.php">Change Password</a></li>
+    </ul>
+</aside>
 
-<aside id="sidebar" class="sidebar">
-				<form method="post" action="">
-        		<button type="submit" name="logout">Logout</button>
-    			</form>
-			  </aside>
+<aside id="sidebar_orders" class="sidebar">
+    <h1>My Orders</h1>
+    <ul class='menu'>
+        <li><a href="<?php echo $SiteUrl ?>/my-waiting-orders">Booking</a></li>
+        <li><a href="<?php echo $SiteUrl ?>/my-confirmed-orders">Order History</a></li>
+    </ul>
+</aside>
 
-			  
+<aside id="sidebar_logout" class="sidebar">
+    <form action="forms/logout.php" method="post">
+        <button type="submit" class="logout-btn" name="logout">Log out</button>
+    </form>
+</aside>
