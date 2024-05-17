64a823438784bc3ea3a2decd6d142813bb2fb439
PuaZiSien <1211210639@student.mmu.edu.my>
2024-05-17 19:02:41 +0800

    Pua

--- user/forgot-password.php
+++ user/forgot-password.php
@@ -1,57 +1,60 @@
 <?php
 include("lib/db.php");
+include("lib/function.php");
+
+$function = new Functions;
 
 $TITLE = "Forgot Your Password?";
-$SiteTitle = "YourSiteTitle";
-$SiteUrl = "http://yourwebsite.com"; 
+$SiteUrl = "http://localhost:80/FYP-Project";
 
-$msg = isset($_POST["msg"]) ? $_POST["msg"] : [];
-$PassEmail = isset($_POST["PassEmail"]) ? $_POST["PassEmail"] : "";
+$msg = isset($_GET["msg"]) ? $_GET["msg"] : ""; // Check if 'msg' exists in the GET request
+$PassEmail = "";
 $content = "";
 
-if(isset($_POST["BtnPass"])) {
-    $PassEmail = $_POST["PassEmail"];
+if(isset($_POST["BtnPass"]))
+{
+	$PassEmail = $_POST["PassEmail"];
 
-    $msg = [];
+	$msg = array();
 
-    // Sanitize input
-    $PassEmail = mysqli_real_escape_string($db_conn, $PassEmail);
+	$check_query = mysqli_query($db_conn, "SELECT MemberID, MemberName FROM member WHERE MemberLogin='".mysqli_escape_string($db_conn, $PassEmail)."' AND isUp='1'");
+	$check_num = mysqli_num_rows($check_query);
 
-    $check_query = mysqli_prepare($db_conn, "SELECT MemberID, MemberName FROM member WHERE MemberEmail=? AND isUp='1'");
-    mysqli_stmt_bind_param($check_query, "s", $PassEmail);
-    mysqli_stmt_execute($check_query);
-    mysqli_stmt_store_result($check_query);
-    $check_num = mysqli_stmt_num_rows($check_query);
+	if($check_num == 0)
+		$msg[] = "Invalid Email Address.";
+	else
+	{
+		$check_row = mysqli_fetch_array($check_query);
+		
+		$MemberID = $check_row["MemberID"];
+		$MemberName = stripslashes($check_row["MemberName"]);
+		
+		$encodedToken = substr_replace($PassEmail, $MemberID."#".date("His"), 4, 0);
 
-    if($check_num == 0) {
-        $msg[] = "Invalid Email Address.";
-    } else {
-        mysqli_stmt_bind_result($check_query, $MemberID, $MemberName);
-        mysqli_stmt_fetch($check_query);
+$resetLink = "$SiteUrl/reset-password.php?token=$encodedToken";
 
-        $encodedToken = substr_replace($PassEmail, $MemberID."#".date("His"), 4, 0);
-
-        // Store encoded token in the database
-        $encodedToken = mysqli_real_escape_string($db_conn, $encodedToken);
-        mysqli_query($db_conn, "UPDATE member SET MemberReset='$encodedToken' WHERE MemberID='$MemberID'");
-
-        // Send email with reset link
-        $resetLink = "http://yourwebsite.com/reset-password.php?token=$encodedToken";
-        $to = $PassEmail;
-        $subject = "Password Reset";
-        $message = "Click the link below to reset your password:\n\n$resetLink";
-        $headers = "From: your@example.com" . "\r\n" .
-           "Reply-To: your@example.com" . "\r\n";
-
-
-        if (mail($to, $subject, $message, $headers)) {
-            $msg[] = "Password reset link has been sent to your email.";
-        } else {
-            $msg[] = "Failed to send the password reset email. Please try again later.";
-        }
-    }
+$custom_msg = "$resetLink######$PassEmail";
+		
+		$function->send_email("2", $MemberName, $PassEmail, $custom_msg);
+		
+		mysqli_query($db_conn, "UPDATE member SET MemberReset=DATE_ADD(NOW(), INTERVAL 8 HOUR) WHERE MemberID='$MemberID'");
+		
+		$content .= "<script>self.location = '$SiteUrl/forgotpass/success/'</script>";
+	}
+	
+	if(!empty($msg))
+		$type = "error";
 }
 
+if(!empty($msg))
+{
+	switch($msg)
+	{
+		case "success": $type = "success"; $msg = "An email have sent to your email address."; break;
+		case "expired": $type = "error"; $msg = "The link is not longer valid."; break;
+		case "invalid": $type = "error"; $msg = "Invalid link."; break;
+	}
+}
 ?>
 
 <!DOCTYPE html>
@@ -68,9 +71,13 @@ if(isset($_POST["BtnPass"])) {
             <div class="row">
                 <h3 class="mb-30"><span>Forgot Password</span></h3>
                 <?php 
-                if(!empty($msg)) {
-                    foreach ($msg as $message) {
-                        echo "<p>$message</p>";
+                if (!empty($msg)) {
+                    if (is_array($msg)) {
+                        foreach ($msg as $message) {
+                            echo "<p>$message</p>";
+                        }
+                    } else {
+                        echo "<p>$msg</p>";
                     }
                 }
                 ?>
