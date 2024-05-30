c4888a0b4963ff09b9befe3d0a6ef6a94ce97dc1
KO XUE NING <1211208724@student.mmu.edu.my>
2024-05-31 01:00:42 +0800

    KO

--- admin/lib/navbar.php
+++ admin/lib/navbar.php
@@ -94,7 +94,7 @@ function timeElapsedString($datetime, $full = false) {
                                                 <img src="../upload/product/<?php echo $ProID; ?>/<?php echo $image_url; ?>" alt="" class="user-avatar-md rounded-circle">
                                             </div>
                                             <div class="notification-list-user-block">
-                                                <span class="notification-list-user-name" style="color:red;"><?php echo $productRow["ProID"]; ?></span> needs to be restocked.
+                                                <span class="notification-list-user-name" style="color:red;"><?php echo $productRow["ProName"]; ?></span>needs to be restocked.
                                                 <div class="notification-date"><?php echo timeElapsedString($dateToUse); ?></div>
                                             </div>
                                         </div>
